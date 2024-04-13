c46dbd4686e8f857e2639be690a164c1a9146403
reeveboy <reeveboy10@gmail.com>
2024-04-13 13:15:48 +0100

    UI Improvement

--- app/page.tsx
+++ app/page.tsx
@@ -46,15 +46,18 @@ export default function Home() {
 
   return (
     <div className="container mx-auto px-4 py-8 ">
-      <div className="flex flex-col p-4 items-center">
-        <pre>{art}</pre>
-        <form onSubmit={handleSubmit} className="flex gap-2">
+      <div className="flex flex-col items-center">
+        <pre className="text-xs md:text-md">{art}</pre>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col w-full md:flex-row gap-2 p-4 justify-center"
+        >
           <input
             type="text"
             value={query}
             onChange={(e) => setQuery(e.target.value)}
             placeholder="Enter your search query"
-            className="text-sm w-80 rounded border border-stone-400 bg-stone-100 px-2 py-2 focus:outline-none focus:ring-2 focus:ring-sky-700"
+            className="text-sm md:w-80 rounded border border-stone-400 bg-stone-100 px-2 py-2 focus:outline-none focus:ring-2 focus:ring-sky-700"
           />
           <button
             type="submit"
@@ -67,18 +70,20 @@ export default function Home() {
 
       {/* Display search results here (conditional rendering based on results) */}
       {searchResults.length > 0 && (
-        <div className="mt-4">
-          <p>Showing top {searchResults.length} results:</p>
-          <div className="grid grid-cols-2 md:grid-cols-3 gap-2">
+        <div className="mt-2 p-4">
+          <p className="text-xs md:text-sm">
+            Showing top {searchResults.length} results:
+          </p>
+          <div className="masonry sm:masonry-sm md:masonry-md mt-2">
             {/* Map over searchResults and render each result */}
             {searchResults.map((result) => (
-              <div key={result.index} className="rounded p-4">
+              <div key={result.index} className="overflow-hidden mb-[2em]">
                 <img
                   src={result.image_url}
                   alt="search result"
-                  className="rounded w-full h-48 object-cover"
+                  className="w-full h-full object-cover rounded"
                 />
-                <p className="text-sm mt-2">{result.summary}</p>
+                <p className="text-xs mt-1">{result.summary}</p>
               </div>
             ))}
           </div>
